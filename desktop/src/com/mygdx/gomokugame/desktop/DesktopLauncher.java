663a16812ee065d35cec6b5f09066e0305962aa3
Alex <rikka.gold@mail.ru>
2019-03-03 00:28:01 +0300

    Initial commit

--- desktop/src/com/mygdx/gomokugame/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/gomokugame/desktop/DesktopLauncher.java
@@ -9,7 +9,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 800;
-		config.height = 800;
+		config.height = 600;
 		new LwjglApplication(new MenuWindow(), config);
 	}
 }
